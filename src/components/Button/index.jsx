4c67bb7c9a2e96d0a7f87336b6916aae92032f67
fmaulll <maul2821@gmail.com>
2024-02-12 23:55:13 +0700

    edit button

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
-const Button = ({ children, onClick }) => {
-  return <button className="font-semibold py-4 bg-white rounded-lg border-none outline-none w-full" onClick={onClick}>{children}</button>;
+const Button = ({ children, onClick, type, className }) => {
+  let btnType = "bg-white"
+  if (type === "gray") {
+    btnType = "bg-[#D9E3DE] hover:bg-[#b6bfba]"
+  }
+  if (type === "orange") {
+    btnType = "bg-[#ffa140] hover:bg-[#FFB66B] text-white"
+  }
+  return <button className={`${btnType} font-semibold py-4 px-[25px] rounded-lg border-none outline-none w-full ${className}`} onClick={onClick}>{children}</button>;
 };
 
 export default Button;
