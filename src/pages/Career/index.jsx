a921bf65a01adcdd39c32f0134b1e0fe45cb1846
fmaulll <maul2821@gmail.com>
2024-02-14 19:40:20 +0700

    finish career

--- src/pages/Career/index.jsx
+++ src/pages/Career/index.jsx
@@ -1,8 +1,8 @@
-import { createClient } from '@supabase/supabase-js';
-import React, { useContext, useEffect, useState } from 'react'
-import { useNavigate } from 'react-router-dom';
-import Button from '../../components/Button';
-import { LayoutContext } from '../../context/LayoutContext';
+import { createClient } from "@supabase/supabase-js";
+import React, { Fragment, useContext, useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+import Button from "../../components/Button";
+import { LayoutContext } from "../../context/LayoutContext";
 
 // Create a single supabase client for interacting with your database
 const supabase = createClient(
@@ -18,7 +18,7 @@ const Career = () => {
 
   const getCareerData = async () => {
     setLoading(true);
-    const { data, error } = await supabase.from("Career").select();
+    const { data, error } = await supabase.from("Career").select().eq('active', true);
     if (data && data.length > 0) {
       setCareerData(data);
     }
@@ -31,31 +31,53 @@ const Career = () => {
     }
     setLoading(false);
   };
-  
+
   useEffect(() => {
     getCareerData();
   }, []);
   return (
-    <div>
-      <p className='text-xl'>Tersedia <span className='font-bold'>{careerData.length} Job</span></p>
-      {careerData.map((item, index) => (
-        <div key={index} className='flex justify-between text-xl py-10 border-b'>
-          <span>{item.role}</span>
-          <div className='flex items-center'>
-            <div className='text-center mr-[72px]'>
-              <span className='font-extrabold text-[#BCBCBC]'>Tipe Pekerjaan</span><br/>
-              <span>{item.job_type}</span>
+    <div className="pt-[180px] px-[110px]">
+      {careerData.length > 0 && <p className="text-xl">
+        Tersedia <span className="font-bold">{careerData.length} Job</span>
+      </p>}
+      {careerData.length > 0 ? (
+        <Fragment>
+          {careerData.map((item, index) => (
+            <div
+              key={index}
+              className="flex justify-between text-xl py-10 border-b"
+            >
+              <span>{item.role}</span>
+              <div className="flex items-center">
+                <div className="text-center mr-[72px]">
+                  <span className="font-extrabold text-[#BCBCBC]">
+                    Tipe Pekerjaan
+                  </span>
+                  <br />
+                  <span>{item.job_type}</span>
+                </div>
+                <div className="text-center mr-[72px]">
+                  <span className="font-extrabold text-[#BCBCBC]">
+                    Sistem Kerja
+                  </span>
+                  <br />
+                  <span>{item.working_arrangement}</span>
+                </div>
+                <Button
+                  type="gray"
+                  onClick={() => navigate(`/career/details/${item.id}`)}
+                >
+                  Detail
+                </Button>
+              </div>
             </div>
-            <div className='text-center mr-[72px]'>
-              <span className='font-extrabold text-[#BCBCBC]'>Sistem Kerja</span><br/>
-              <span>{item.working_arrangement}</span>
-            </div>
-            <Button type="gray" onClick={() => navigate(`/career/details/${item.id}`)}>Detail</Button>
-          </div>
-        </div>
-      ))}
+          ))}
+        </Fragment>
+      ) : (
+        <p className="text-center text-xl text-[#7D7D7D]">No Job Available</p>
+      )}
     </div>
-  )
-}
+  );
+};
 
-export default Career
+export default Career;
