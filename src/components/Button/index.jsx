9bffea589da8fdd9c502ee14fced5d27c8c7073d
fmaulll <maul2821@gmail.com>
2024-02-17 14:06:24 +0700

    update btn

--- src/components/Button/index.jsx
+++ src/components/Button/index.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-const Button = ({ children, onClick, type, className }) => {
-  let btnType = "bg-white"
+const Button = ({ children, onClick, type, className, ...other }) => {
+  let btnType = "bg-white disabled:bg-gray-400 disabled:cursor-not-allowed"
   if (type === "gray") {
     btnType = "bg-[#D9E3DE] hover:bg-[#b6bfba]"
   }
   if (type === "orange") {
     btnType = "bg-[#ffa140] hover:bg-[#FFB66B] text-white"
   }
-  return <button className={`${btnType} whitespace-nowrap font-semibold rounded-lg border-none outline-none ${className ? className : 'py-4 px-6 w-min'}`} onClick={onClick}>{children}</button>;
+  return <button {...other} className={`${btnType} whitespace-nowrap font-semibold rounded-lg border-none outline-none ${className ? className : 'py-4 px-6 w-min'}`} onClick={onClick}>{children}</button>;
 };
 
 export default Button;
