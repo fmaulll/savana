ebee399071414648912c74f5e85f05047a302f49
fmaulll <maul2821@gmail.com>
2024-02-26 23:26:25 +0700

    damn

--- src/pages/AdminPages/AddEditProject.jsx
+++ src/pages/AdminPages/AddEditProject.jsx
@@ -7,6 +7,10 @@ import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { supabase } from "../../hooks/supabase";
 import { useEffect } from "react";
+import { SlPaperClip } from "react-icons/sl";
+import { FaRegTrashAlt } from "react-icons/fa";
+import InputDate from "../../components/InputDate";
+import { MdOutlineAddPhotoAlternate } from "react-icons/md";
 
 const initialValue = {
   start_date: "",
@@ -31,80 +35,10 @@ const AddEditProject = () => {
   const [dataRequest, setDataRequest] = useState(initialValue);
   const [documentation, setDocumentation] = useState([]);
 
-  const uploadTableImages = async (array) => {
-    const { error } = await supabase.from("countries").insert([array]);
-
-    return error;
-  };
-
-  const uploadImages = async (file) => {
-    let file_data = { path: "", url: "", file_name: file.name };
-    const { data, error } = await supabase.storage
-      .from("savana")
-      .upload("project/" + file.name, file, {
-        cacheControl: "3600",
-        upsert: false,
-      });
-
-    if (data) {
-      file_data = {
-        ...file_data,
-        path: data.path,
-        file_name: getPhotosUrl(data.path),
-      };
-      return file_data;
-    }
-
-    if (error) {
-      setLoading(false);
-      setMessage(error.message);
-      setStatus(false);
-      return "";
-    }
-
-    setLoading(false);
-    return "";
-  };
-
-  const getPhotosUrl = (path) => {
-    const { data } = supabase.storage.from("savana").getPublicUrl(path);
-
-    return data.publicUrl;
-  };
-
-  const publishOrEdit = async (dataValue) => {
-    if (type === "edit") {
-      const { error } = await supabase
-        .from("projects")
-        .update(dataValue)
-        .eq("id", id);
-
-      return error;
-    } else {
-      const { error } = await supabase.from("project").insert(dataValue);
-
-      return error;
-    }
-  };
-
-  const getCareerData = async () => {
-    setLoading(true);
-    const { data, error } = await supabase
-      .from("Career")
-      .select()
-      .eq("id", id)
-      .single();
-    if (data) {
-      setDataRequest(data);
-    }
-
-    if (error) {
-      setLoading(false);
-      setMessage(error.message);
-      setStatus(false);
-      return;
-    }
-    setLoading(false);
+  const handleClickDelete = (index) => {
+    const array = [...documentation];
+    array.splice(index, 1);
+    setDocumentation(array);
   };
 
   const handleChange = (key, value) => {
@@ -116,44 +50,13 @@ const AddEditProject = () => {
     });
   };
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setLoading(true);
-    if (
-      !dataRequest.location ||
-      !dataRequest.role ||
-      !dataRequest.job_type ||
-      !dataRequest.client ||
-      !dataRequest.working_arrangement ||
-      !dataRequest.description ||
-      !dataRequest.requirement
-    ) {
-      setLoading(false);
-      setMessage("Make sure to fill all of the inputs!");
-      setStatus(false);
-      return;
-    }
-
-    let dataValue = dataRequest;
-    let uploadValue = "";
-
-    if (dataValue.file) {
-      uploadValue = await uploadImages(dataValue.file);
-      dataValue = {
-        ...dataValue,
-        file_name: uploadValue.file_name,
-        image_url: uploadValue.url,
-      };
-    }
-
-    dataValue = {
-      ...dataValue,
-      service_id: service_id,
-    };
-
-    delete dataValue.file;
-
-    const error = await publishOrEdit(dataValue);
+  const handleUploadDocumentation = async (file, projectId) => {
+    const { data, error } = await supabase.storage
+      .from("savana")
+      .upload(`project-${projectId}/` + file.name + Date.now(), file, {
+        cacheControl: "3600",
+        upsert: false,
+      });
 
     if (error) {
       setLoading(false);
@@ -161,119 +64,201 @@ const AddEditProject = () => {
       setStatus(false);
       return;
     }
+  };
 
-    const documentationImages = documentation.map((item, index) => {
-      return {
-        image_url: uploadImages(item.file),
-        file_name: item.file.name,
-        project_id: id,
-      };
+  const handleChangeDocumentation = async (file) => {
+    setDocumentation((prev) => {
+      return [...prev, file];
     });
+  };
 
-    if (documentationImages.length > 0) {
-      const errorInsertImages = await uploadTableImages(documentationImages);
+  const getPhotosUrl = (path) => {
+    const { data } = supabase.storage.from("savana").getPublicUrl(path);
 
-      if (errorInsertImages) {
-        setLoading(false);
-        setMessage(errorInsertImages.message);
-        setStatus(false);
-        return;
-      }
+    return data.publicUrl;
+  };
+
+  const handleUploadHighlight = async (file) => {
+    setLoading(true);
+
+    const { data, error } = await supabase.storage
+      .from("savana")
+      .upload("project/" + file.name + Date.now(), file, {
+        cacheControl: "3600",
+        upsert: false,
+      });
+
+    if (data) {
+      return {
+        image_url: getPhotosUrl(data.path),
+        file_name: file.name,
+      };
+    }
+
+    if (error) {
+      setLoading(false);
+      setMessage(error.message);
+      setStatus(false);
+      return {};
     }
 
     setLoading(false);
-    setMessage(type === "edit" ? "Job Edited!" : "Job Created!");
-    setStatus(true);
-    setTimeout(() => {
-      setMessage("");
-      navigate("/admin/karir");
-      return;
-    }, 2000);
   };
 
-  useEffect(() => {
-    if (type === "edit") {
-      getCareerData();
+  const handleDeletePhoto = () => {};
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+
+    const dataHit = {
+      ...dataRequest,
+      service_id: service_id,
+      ...(await handleUploadHighlight(dataRequest.file)),
+    };
+    delete dataHit.file;
+
+    const { data, error } = await supabase
+      .from("projects")
+      .insert(dataHit)
+      .select()
+      .single();
+
+    if (data) {
+      documentation.map((item) => {
+        handleUploadDocumentation(item, data.id);
+      });
     }
-  }, []);
+
+    if (error) {
+      setLoading(false);
+      setMessage(error.message);
+      setStatus(false);
+      return;
+    }
+    navigate(-1);
+    setLoading(false);
+  };
 
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <div className="flex justify-between mb-6">
-          <div className="flex items-center">
-            <input
-              checked={dataRequest.active}
-              id="activeJob"
-              type="checkbox"
-              onChange={(e) =>
-                setDataRequest((prev) => {
-                  return { ...prev, active: e.target.checked };
-                })
-              }
-            />
-            <label className="ml-2" htmlFor="activeJob">
-              Active
+        <div className="flex">
+          <input
+            onChange={(e) => handleChange("file", e.target.files[0])}
+            id="uploadHighlightF"
+            type="file"
+            className="hidden"
+          />
+          <Button>Submit</Button>
+          {dataRequest.file ? (
+            <div
+              className={`mb-3 py-4 px-4 bg-[#D3D3D3] flex items-center rounded-lg w-full rounded-[4px] border-[#929292]`}
+            >
+              <FaRegTrashAlt
+                className="mr-2 bg-red-500 rounded-full p-2 cursor-pointer"
+                fill="white"
+                size={32}
+                onClick={handleDeletePhoto}
+              />
+              <span className="text-[#7C7C7C] font-medium">
+                {dataRequest.file.name.slice(0, 16) + "..."}
+              </span>
+            </div>
+          ) : (
+            <label htmlFor="uploadHighlightF">
+              <div
+                className={`mb-3 py-4 px-4 bg-[#D3D3D3] cursor-pointer flex items-center rounded-lg w-full rounded-[4px] border-[#929292]`}
+              >
+                <SlPaperClip className="mr-2" size={24} />
+                <span className="text-[#7C7C7C] font-medium">
+                  Highlight Photo
+                </span>
+              </div>
             </label>
-          </div>
-          <Button type="orange">{type === "edit" ? "Edit" : "Publish"}</Button>
+          )}
+        </div>
+        <div className="flex">
+          <InputDate
+            onChange={(e) => handleChange("start_date", e.target.value)}
+            className="mr-4 w-[230px]"
+            placeholder="Mulai"
+          />
+          <InputDate
+            onChange={(e) => handleChange("end_date", e.target.value)}
+            className="w-[230px]"
+            placeholder="Selesai"
+          />
         </div>
         <Input
-          value={dataRequest.role}
+          onChange={(e) => handleChange("title", e.target.value)}
+          className="mt-4"
           type="text"
-          placeholder="Posisi Pekerjaan"
-          onChange={(e) => handleChange("role", e.target.value)}
+          placeholder="Judul"
         />
-        <div className="grid grid-cols-2 gap-4 mt-4">
+        <Input
+          onChange={(e) => handleChange("keyword", e.target.value)}
+          className="mt-4"
+          type="text"
+          placeholder="Kata Kunci"
+        />
+        <div className="flex gap-4 mt-4">
           <Input
-            value={dataRequest.job_type}
+            onChange={(e) => handleChange("activity_type", e.target.value)}
             type="text"
-            placeholder="Tipe Pekerjaan"
-            onChange={(e) => handleChange("job_type", e.target.value)}
+            placeholder="Jenis Kegiatan"
           />
           <Input
-            value={dataRequest.working_arrangement}
-            type="text"
-            placeholder="Sistem Kerja"
-            onChange={(e) =>
-              handleChange("working_arrangement", e.target.value)
-            }
-          />
-        </div>
-        <div className="grid grid-cols-2 gap-4 mt-4">
-          <Input
-            value={dataRequest.location}
+            onChange={(e) => handleChange("location", e.target.value)}
             type="text"
             placeholder="Lokasi"
-            onChange={(e) => handleChange("location", e.target.value)}
-          />
-          <Input
-            value={dataRequest.client}
-            type="text"
-            placeholder="Klien"
-            onChange={(e) => handleChange("client", e.target.value)}
           />
         </div>
         <Input
-          value={dataRequest.link}
+          onChange={(e) => handleChange("client", e.target.value)}
           className="mt-4"
           type="text"
-          placeholder="Link"
-          onChange={(e) => handleChange("link", e.target.value)}
+          placeholder="Klien"
         />
+
+        <div className="w-full mt-4">
+          <span className="font-medium">Masukan Foto Dokumentasi</span>
+          <div className="mt-4 h-[306px] w-full border border-[#929292] flex justify-center items-center border-dashed">
+            <div className="grid grid-cols-5 w-full">
+              {documentation.map((item, index) => (
+                <div
+                  key={index}
+                  className="relative border w-full h-[200px] flex justify-center items-center"
+                >
+                  <span>{item.name}</span>
+                  <div
+                    className="absolute top-0 right-0 p-2 bg-red-500 rounded-full cursor-pointer"
+                    onClick={() => handleClickDelete(index)}
+                  >
+                    <FaRegTrashAlt fill="white" />
+                  </div>
+                </div>
+              ))}
+              <label className="cursor-pointer" htmlFor="uploadDocumentation">
+                <div className="border w-full h-[200px] flex justify-center items-center hover:bg-gray-300">
+                  <MdOutlineAddPhotoAlternate size={48} />
+                </div>
+              </label>
+            </div>
+          </div>
+          <input
+            onChange={(e) => handleChangeDocumentation(e.target.files[0])}
+            id="uploadDocumentation"
+            type="file"
+            className="hidden"
+          />
+        </div>
         <ReactQuill
-          className="mt-4"
-          placeholder="Deskripsi Pekerjaan"
+          className="mt-4 mb-10"
+          placeholder="Penjelasan Proyek"
           theme="snow"
           value={dataRequest.description}
           onChange={(e) => handleChange("description", e)}
-        />
-        <ReactQuill
-          className="mt-4 mb-10"
-          placeholder="Persyaratan Pekerjaan"
-          theme="snow"
-          value={dataRequest.requirement}
-          onChange={(e) => handleChange("requirement", e)}
         />
       </form>
     </div>
