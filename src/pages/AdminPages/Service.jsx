8ebecf0c42fcb9b86014e56e74162f9a44f90f8f
fmaulll <maul2821@gmail.com>
2024-02-28 09:28:50 +0700

    order by

--- src/pages/AdminPages/Service.jsx
+++ src/pages/AdminPages/Service.jsx
@@ -6,7 +6,7 @@ import { LayoutContext } from "../../context/LayoutContext";
 import { supabase } from "../../hooks/supabase";
 
 const Service = () => {
-    const navigate = useNavigate()
+  const navigate = useNavigate();
   const { setLoading, setMessage, setStatus, setUser } =
     useContext(LayoutContext);
   const [services, setServices] = useState([]);
@@ -14,7 +14,10 @@ const Service = () => {
   const getServices = async () => {
     setLoading(true);
     const array = [];
-    const { data, error } = await supabase.from("services").select();
+    const { data, error } = await supabase
+      .from("services")
+      .select()
+      .order("created_at");
     if (data && data.length > 0) {
       setServices(array.concat(data));
     }
@@ -35,7 +38,13 @@ const Service = () => {
   return (
     <div className="grid grid-cols-4 w-full gap-x-3 gap-y-6">
       {services.map((item, index) => (
-        <div key={index} onClick={() => navigate(`/admin/pelayanan/${item.id}`)} className="cursor-pointer py-8 text-lg font-medium flex justify-center items-center text-center min-h-[150px] rounded-lg bg-[#D9E3DE] border border-[#B0C6BB]">{item.name}</div>
+        <div
+          key={index}
+          onClick={() => navigate(`/admin/pelayanan/${item.id}`)}
+          className="cursor-pointer py-8 text-lg font-medium flex justify-center items-center text-center min-h-[150px] rounded-lg bg-[#D9E3DE] border border-[#B0C6BB]"
+        >
+          {item.name}
+        </div>
       ))}
     </div>
   );
