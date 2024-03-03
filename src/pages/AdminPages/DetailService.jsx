85622f62e655d7cfcac928acd8a41af818de3513
fmaulll <maul2821@gmail.com>
2024-03-03 15:07:13 +0700

    detail service

--- src/pages/AdminPages/DetailService.jsx
+++ src/pages/AdminPages/DetailService.jsx
@@ -7,21 +7,107 @@ import { LayoutContext } from "../../context/LayoutContext";
 import { supabase } from "../../hooks/supabase";
 import { BiEdit } from "react-icons/bi";
 import { FaRegTrashAlt } from "react-icons/fa";
+import { SlPaperClip } from "react-icons/sl";
+import { getPhotosUrl } from "../../hooks/getUrl";
+import { RiExternalLinkLine } from "react-icons/ri";
 
 const DetailService = () => {
   const { id } = useParams();
   const navigate = useNavigate();
   const { setLoading, setMessage, setStatus, setUser } =
     useContext(LayoutContext);
-  const [serviceDetail, setServiceDetail] = useState(null);
+  const [serviceDetail, setServiceDetail] = useState({
+    description: "",
+    image_url: "",
+    file_name: "",
+    file: "",
+  });
   const [projects, setProjects] = useState([]);
 
-  const getProjects = async() => {
+  const deletePhotoFromService = async () => {
+    setLoading(true);
+    const { error } = await supabase
+      .from("services")
+      .update({ image_url: "", file_name: "" })
+      .eq("id", id);
+    if (error) {
+      setLoading(false);
+      setMessage(error.message);
+      setStatus(false);
+      return;
+    }
+    getServiceDetail();
+    setLoading(false);
+  };
+
+  const handleDeletePhoto = async (file) => {
+    setLoading(true);
+    const { data, error } = await supabase.storage
+      .from("savana")
+      .remove(["service/" + file]);
+
+    if (data) {
+      deletePhotoFromService();
+    }
+
+    if (error) {
+      setLoading(false);
+      setMessage(error.message);
+      setStatus(false);
+      return;
+    }
+    setLoading(false);
+  };
+
+  const insertServicePhoto = async (dataRequest) => {
+    setLoading(true);
+    const { error } = await supabase
+      .from("services")
+      .update(dataRequest)
+      .eq("id", id);
+
+    if (error) {
+      setLoading(false);
+      setMessage(error.message);
+      setStatus(false);
+      return;
+    }
+    getServiceDetail();
+    setLoading(false);
+  };
+
+  const handleUpload = async (file) => {
+    setLoading(true);
+    const fileName = file.name + Date.now();
+    const { data, error } = await supabase.storage
+      .from("savana")
+      .upload(`service/${fileName}`, file, {
+        cacheControl: "3600",
+        upsert: false,
+      });
+
+    if (data) {
+      const dataRequest = {
+        image_url: getPhotosUrl(data.path),
+        file_name: fileName,
+      };
+      insertServicePhoto(dataRequest);
+    }
+    if (error) {
+      setLoading(false);
+      setMessage(error.message);
+      setStatus(false);
+      return;
+    }
+    setLoading(false);
+  };
+
+  const getProjects = async () => {
     setLoading(true);
     const { data, error } = await supabase
       .from("projects")
       .select()
-      .eq("service_id", id)
+      .eq("service_id", id);
     if (data) {
       setProjects(data);
     }
@@ -33,7 +119,7 @@ const DetailService = () => {
       return;
     }
     setLoading(false);
-  }
+  };
 
   const getServiceDetail = async () => {
     setLoading(true);
@@ -57,13 +143,68 @@ const DetailService = () => {
 
   useEffect(() => {
     getServiceDetail();
-    getProjects();  
+    getProjects();
   }, []);
   return (
     <Fragment>
-      {!serviceDetail || !serviceDetail.name  ? "" : <div className="w-full font-bold text-2xl">{serviceDetail.name}</div>}
+      {!serviceDetail || !serviceDetail.name ? (
+        ""
+      ) : (
+        <div className="w-full font-bold text-2xl">{serviceDetail.name}</div>
+      )}
       <div className="mt-4">
-        <Button type="gray" onClick={() => navigate(`/admin/pelayanan/description/service/${id}`)}>Edit</Button>
+        <input
+          onChange={(e) => handleUpload(e.target.files[0])}
+          id="uploadHighlight"
+          type="file"
+          className="hidden"
+        />
+        {serviceDetail.image_url || serviceDetail.file ? (
+          <div
+            className={`mb-3 py-4 px-4 bg-[#D3D3D3] flex items-center rounded-lg w-min rounded-[4px] border-[#929292]`}
+          >
+            <FaRegTrashAlt
+              className="mr-2 bg-red-500 rounded-full p-2 cursor-pointer"
+              fill="white"
+              size={32}
+              onClick={() => handleDeletePhoto(serviceDetail.file_name)}
+            />
+            <span
+              onClick={() => {
+                if (serviceDetail.image_url) {
+                  window.open(serviceDetail.image_url);
+                }
+              }}
+              className={`text-[#7C7C7C] font-medium flex ${
+                serviceDetail.image_url ? "cursor-pointer" : ""
+              }`}
+            >
+              {serviceDetail.image_url
+                ? serviceDetail.file_name.slice(0, 16) + "..."
+                : serviceDetail.file.name.slice(0, 16) + "..."}{" "}
+              {serviceDetail.image_url && (
+                <RiExternalLinkLine size={24} className="ml-2" />
+              )}
+            </span>
+          </div>
+        ) : (
+          <label htmlFor="uploadHighlight">
+            <div
+              className={`mb-3 py-4 px-4 bg-[#D3D3D3] cursor-pointer flex items-center rounded-lg w-full rounded-[4px] border-[#929292] w-min whitespace-nowrap`}
+            >
+              <SlPaperClip className="mr-2" size={24} />
+              <span className="text-[#7C7C7C] font-medium">
+                Highlight Photo
+              </span>
+            </div>
+          </label>
+        )}
+        <Button
+          type="gray"
+          onClick={() => navigate(`/admin/pelayanan/description/service/${id}`)}
+        >
+          Edit
+        </Button>
         {!serviceDetail || !serviceDetail.description ? (
           <div className="text-center mt-3">No description</div>
         ) : (
@@ -116,7 +257,9 @@ const DetailService = () => {
                   <div className="flex w-full h-full justify-center items-center">
                     <BiEdit
                       className="cursor-pointer mr-2"
-                      onClick={() => navigate(`/admin/pelayanan/edit/${id}/${item.id}`)}
+                      onClick={() =>
+                        navigate(`/admin/pelayanan/edit/${id}/${item.id}`)
+                      }
                       size={30}
                     />
                     <FaRegTrashAlt
