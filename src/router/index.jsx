8dee14dfc4d1f0c32bc92682dc520db26d2d30cf
fmaulll <maul2821@gmail.com>
2024-03-12 06:20:31 +0700

    add new path

--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,8 +1,8 @@
-import Home from "../pages/Home"
-import Pelayanan from "../pages/Pelayanan"
-import Career from "../pages/Career"
-import Dashboard from "../pages/AdminPages/Dashboard"
-import AdminLogin from "../pages/AdminLogin"
+import Home from "../pages/Home";
+import Pelayanan from "../pages/Pelayanan";
+import Career from "../pages/Career";
+import Dashboard from "../pages/AdminPages/Dashboard";
+import AdminLogin from "../pages/AdminLogin";
 import CareerAdmin from "../pages/AdminPages/CareerAdmin";
 import AddCareerAdmin from "../pages/AdminPages/AddCareerAdmin";
 import Details from "../pages/Career/Details";
@@ -13,83 +13,94 @@ import EditServiceDescription from "../pages/AdminPages/EditServiceDescription";
 import AboutUs from "../pages/AdminPages/AboutUs";
 import AboutUsDetail from "../pages/AdminPages/AboutUsDetail";
 import AddEditEmployee from "../pages/AdminPages/AddEditEmployee";
+import DetailPelayanan from "../pages/Pelayanan/DetailPelayanan";
+import DetailProyek from "../pages/Proyek/DetailProyek";
+import Proyek from "../pages/Proyek";
 
 export const NonAuthRoutes = [
-    {
-        path: "/",
-        component: <Home />
-    },
-    {
-        path: "/services",
-        component: <Pelayanan />
-    },
-    {
-        path: "/news",
-        component: <div>asd</div>
-    },
-    {
-        path: "/career",
-        component: <Career />
-    },
-    {
-        path: "/career/details/:id",
-        component: <Details />
-    },
-    {
-        path: "/about",
-        component: <div>asd</div>
-    },
-    {
-        path: "/admin/login",
-        component: <AdminLogin />
-    },
-]
+  {
+    path: "/",
+    component: <Home />,
+  },
+  {
+    path: "/services",
+    component: <Pelayanan />,
+  },
+  {
+    path: "/services/detail/:id",
+    component: <DetailPelayanan />,
+  },
+  {
+    path: "/article",
+    component: <Proyek />,
+  },
+  {
+    path: "/article/project/:id",
+    component: <DetailProyek />,
+  },
+  {
+    path: "/career",
+    component: <Career />,
+  },
+  {
+    path: "/career/details/:id",
+    component: <Details />,
+  },
+  {
+    path: "/about",
+    component: <div>asd</div>,
+  },
+  {
+    path: "/admin/login",
+    component: <AdminLogin />,
+  },
+];
 
-// dynamic sidebar menu 
+// dynamic sidebar menu
 
 export const AuthRoutes = [
-    {
-        path: "/admin/dashboard",
-        component: <Dashboard />,
-    },
-    {
-        path: "/admin/karir",
-        component: <CareerAdmin />,
-    },
-    {
-        path: "/admin/karir/:type/:id",
-        component: <AddCareerAdmin />,
-    },
-    {
-        path: "/admin/pelayanan",
-        component: <Service />,
-    },
-    {
-        path: "/admin/pelayanan/:id",
-        component: <DetailService />,
-    },
-    {
-        path: "/admin/pelayanan/:type/:service_id/:id",
-        component: <AddEditProject />,
-    },
-    {
-        path: "/admin/pelayanan/description/:type/:id",
-        component: <EditServiceDescription />,
-    },
-    {
-        path: "/admin/about",
-        component: <AboutUs />,
-    },
-    {
-        path: "/admin/about/:id",
-        component: <AboutUsDetail />,
-    },
-    {
-        path: "/admin/about/employee/:type/:id",
-        component: <AddEditEmployee />,
-    },
-    {
-        path: "/admin/about/employee/:type",
-        component: <AddEditEmployee />,
-    },
-]
+  {
+    path: "/admin/dashboard",
+    component: <Dashboard />,
+  },
+  {
+    path: "/admin/karir",
+    component: <CareerAdmin />,
+  },
+  {
+    path: "/admin/karir/:type/:id",
+    component: <AddCareerAdmin />,
+  },
+  {
+    path: "/admin/pelayanan",
+    component: <Service />,
+  },
+  {
+    path: "/admin/pelayanan/:id",
+    component: <DetailService />,
+  },
+  {
+    path: "/admin/pelayanan/:type/:service_id/:id",
+    component: <AddEditProject />,
+  },
+  {
+    path: "/admin/pelayanan/description/:type/:id",
+    component: <EditServiceDescription />,
+  },
+  {
+    path: "/admin/about",
+    component: <AboutUs />,
+  },
+  {
+    path: "/admin/about/:id",
+    component: <AboutUsDetail />,
+  },
+  {
+    path: "/admin/about/employee/:type/:id",
+    component: <AddEditEmployee />,
+  },
+  {
+    path: "/admin/about/employee/:type",
+    component: <AddEditEmployee />,
+  },
+];
